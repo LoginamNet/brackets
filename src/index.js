a59c87ad88255feaa12227c234bc32cc0e6f1834
LoginamNet <loginamnet@gmail.com>
2022-06-23 10:36:32 +0300

    refactor: dont work for similar try another way

--- src/index.js
+++ src/index.js
@@ -1,35 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length < 2) {
-    return false
-  };
-    
-  let openers = [];
-  let closers = [];
-    
-    for (let arr of bracketsConfig) {
-      openers.push(arr[0]);
-      closers.push(arr[1]);
-    }
-    
-  let s = [];
-  let bracket;
-  let openBracket;
-  
-  for (let i = 0; i < str.length; i++) {
-      
-      bracket = str[i];
-      
-      if (closers.indexOf(bracket) > -1) {
-        openBracket = openers[closers.indexOf(bracket)]
-        
-        if (s.length == 0 || (s.pop() != openBracket)) {
-          return false;
-        }
+  bracketsConfig = bracketsConfig.map(item => item.join(''));
+
+  while (bracketsConfig.length) {
+    let iterator = 0;
+
+    bracketsConfig.forEach(item => {
+      if (str.indexOf(item) != -1) {
+        str = str.replace(item, '');
       } else {
-        s.push(bracket);
+        iterator += 1;
       }
+    });
+    
+    if (iterator === bracketsConfig.length) {
+      bracketsConfig.length = 0;
     }
-  
-    return (s.length == 0)
+  }
+  return str.length === 0 ? true : false;
   
 }
