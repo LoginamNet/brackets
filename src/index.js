006e02d54229ff87289747f8c38131bb257e4f6a
LoginamNet <loginamnet@gmail.com>
2022-06-23 11:16:34 +0300

    refactor: remede with while to work instead of for

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   
-  let pairs = bracketsConfig.map(arr => arr.join('')); 
-  //создаем пары вида () из массивов "скобок"
-    
-    for (let i = 0; i < str.length; i++) {
+let pairs = bracketsConfig.map(arr => arr.join('')); 
+//создаем пары вида () из массивов "скобок"
   
-      for (let item of pairs) {
-        if (str.indexOf(item) > -1) {
-          str = str.replace(item, '')
-          console.log(str)
-        }
-      }
-  //если в строке есть любая пара из pairs, то "удаляем" её, заменяя на ''. И так в цикле
-      
+  while (pairs.length) {
+
+    let i = 0;
+
+    for (let item of pairs) {
+      if (str.indexOf(item) > -1) {
+        str = str.replace(item, '')
+        console.log(str)
+      } else {i++}
     }
-    return (!str) ? true : false;
-  //если после всех манипуляций длина строки равна 0, то всё верно, если нет, то ложно
-  } 
+
+    if (i === pairs.length) pairs.length = 0;  
+  }
+//если в строке есть любая пара из pairs, то "удаляем" её, заменяя на ''. И так в цикле пока не продём все пары
+
+  return (!str) ? true : false;
+//если после всех манипуляций длина строки равна 0, то всё верно, если нет, то ложно
+} 
