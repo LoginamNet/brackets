f6ec97030a05c6acf7a79306c9ade9726d299d44
LoginamNet <loginamnet@gmail.com>
2022-06-23 11:07:06 +0300

    fix: trying to fix errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   
-  let pairs = bracketsConfig.map(arr => arr.join('')); //создаем пары вида () из массивов "скобок"
-  
-  for (let i = 0; i < str.length; i++) {
-
-    for (let item of pairs) {
-      if (str.indexOf(item) > -1) {
-        str.replace(item, '')
-      }
-    }
+  let pairs = bracketsConfig.map(arr => arr.join('')); 
+  //создаем пары вида () из массивов "скобок"
     
-  }
-  return (!str) ? true : false;
-
-}
+    for (let i = 0; i < str.length; i++) {
+  
+      for (let item of pairs) {
+        if (str.indexOf(item) > -1) {
+          str = str.replace(item, '')
+          console.log(str)
+        }
+      }
+  //если в строке есть любая пара из pairs, то "удаляем" её, заменяя на ''. И так в цикле
+      
+    }
+    return (!str) ? true : false;
+  //если после всех манипуляций длина строки равна 0, то всё верно, если нет, то ложно
+  } 
