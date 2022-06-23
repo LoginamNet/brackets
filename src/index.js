15bbb70f867969e0a80ff85dcdd2008ff1a38d11
LoginamNet <loginamnet@gmail.com>
2022-06-23 09:51:31 +0300

    feat: add function code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length < 2) {
+    return false
+  };
+    
+  let openers = [];
+  let closers = [];
+    
+    for (let arr of bracketsConfig) {
+      openers.push(arr[0]);
+      closers.push(arr[1]);
+    }
+    
+  let s = [];
+  let bracket;
+  let openBracket;
+  
+  for (let i = 0; i < str.length; i++) {
+      
+      bracket = str[i];
+      
+      if (closers.indexOf(bracket) > -1) {
+        openBracket = openers[closers.indexOf(bracket)]
+        
+        if (s.length == 0 || (s.pop() != openBracket)) {
+          return false;
+        }
+      } else {
+        s.push(bracket);
+      }
+    }
+  
+    return (s.length == 0)
+  
 }
