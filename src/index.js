98f14ee6744c3eab4a1b9b21d43da7dc862ecd6c
LoginamNet <loginamnet@gmail.com>
2022-06-23 10:52:18 +0300

    refactor: new code with "" replace

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  bracketsConfig = bracketsConfig.map(item => item.join(''));
-
-  while (bracketsConfig.length) {
-    let iterator = 0;
-
-    bracketsConfig.forEach(item => {
-      if (str.indexOf(item) != -1) {
-        str = str.replace(item, '');
-      } else {
-        iterator += 1;
-      }
-    });
-    
-    if (iterator === bracketsConfig.length) {
-      bracketsConfig.length = 0;
-    }
-  }
-  return str.length === 0 ? true : false;
   
+  let pairs = bracketsConfig.map(arr => arr.join('')); //создаем пары вида () из массивов "скобок"
+  
+  for (let i = 0; i < str.length; i++) {
+
+    for (let item of pairs) {
+      if (str.indexOf(item) > -1) {
+        str.replace(item, '')
+      }
+    }
+    
+  }
+  return (!str) ? true : false;
+
 }
